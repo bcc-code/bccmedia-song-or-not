0f00792d8c2d6afed539f3e9f92a5e5327cccd32
Fredrik Vedvik <fredrik@vedvik.tech>
2023-06-30 13:53:37 +0200

    include .pt

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='song_or_not',
-    version='1.0.4',
+    version='1.0.5',
     description='Detect if audio is song or speech',
     author='BCC Media',
     author_email='support@bcc.media',
     url='https://github.com/bcc-code/bccmedia-song-or-not',
     packages=find_packages(exclude=['tests', 'songs', 'speech']),
+    package_data={
+        'inference': ['*.pt'],
+    },
     install_requires=["torch", "torchaudio"]
 )
