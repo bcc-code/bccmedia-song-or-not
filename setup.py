071964c792c3f6a1cd9d43b7098055a3a21647f9
Fredrik Vedvik <fredrik@vedvik.tech>
2023-06-30 12:30:41 +0200

    setup tweaks

--- setup.py
+++ setup.py
@@ -1,33 +1,11 @@
-import os
-import platform
-import sys
-
-import pkg_resources
-from setuptools import find_packages, setup
-
-requirements = []
-if sys.platform.startswith("linux") and platform.machine() == "x86_64":
-    requirements.append("triton==2.0.0")
+from distutils.core import setup
 
 setup(
-    name="bccmedia-song-or-not",
-    py_modules=["inference"],
-    version="1.0.0",
-    description="Robust Speech Recognition via Large-Scale Weak Supervision",
-    long_description=open("readme.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    readme="readme.md",
-    python_requires=">=3.8",
-    author="BCC Code",
-    url="https://github.com/bcc-code/bccmedia-song-or-not",
-    license="MIT",
-    packages=find_packages(),
-    install_requires=requirements
-                     + [
-                         str(r)
-                         for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-                     ],
-    include_package_data=True,
+    name='BCC Media - Song or Not',
+    version='1.0.2',
+    description='Detect if audio is song or speech',
+    author='BCC Media',
+    author_email='support@bcc.media',
+    url='https://github.com/bcc-code/bccmedia-song-or-not',
+    packages=['classifier', 'inference'],
 )
